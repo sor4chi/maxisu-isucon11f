8b45f9dbc0f4af8690f46e5458233f1dca59eb22
sor4chi <sorakawa0218@gmail.com>
2023-09-23 14:46:09 +0900

    fix: total scoreのNOT NULL制約を外す

--- sql/4_add_total_score.sql
+++ sql/4_add_total_score.sql
@@ -1,3 +1,2 @@
 ALTER TABLE `registrations` ADD COLUMN `total_score` TINYINT UNSIGNED;
 UPDATE `registrations` SET `total_score` = 0;
-ALTER TABLE `registrations` MODIFY COLUMN `total_score` TINYINT UNSIGNED NOT NULL;
